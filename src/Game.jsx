fde2c09b13312300dd4a6c330dd5536b998de3ba
João Felipe Zini <joaofelipezini@gmail.com>
2023-04-09 14:32:18 -0300

    style: fixing lint issues

--- src/Game.jsx
+++ src/Game.jsx
@@ -18,7 +18,7 @@ export default class Game extends Component {
     cards: {
       playerCard: 0,
       enemyCard: 1,
-    }
+    },
   };
 
   componentDidMount() {
@@ -33,21 +33,20 @@ export default class Game extends Component {
 
   startGame = () => {
     const { cards } = this.props;
-    const {cards: { enemyCard }} = this.state;
+    const { cards: { enemyCard } } = this.state;
     if (enemyCard < cards.length - 2) {
-      this.setState(prevState => ({
+      this.setState((prevState) => ({
         contestantsReady: true,
         resolution: { msg: 'Selecione uma categoria' },
         hideEnemyCard: true,
         cards: {
           playerCard: prevState.cards.playerCard + 2,
           enemyCard: prevState.cards.enemyCard + 2,
-        }
+        },
       }));
     } else {
-      this.setState({resolution: { msg: 'FIM DE JOGO!' }})
+      this.setState({ resolution: { msg: 'FIM DE JOGO!' } });
     }
-
   };
 
   winner = (message, cond) => {
@@ -57,28 +56,28 @@ export default class Game extends Component {
   fight = (pAttr, cardAttr) => {
     if (pAttr > cardAttr) {
       this.winner('Shantay you stay', 'win');
-      this.setState(prevState => ({
-        score: { 
+      this.setState((prevState) => ({
+        score: {
           player: prevState.score.player + 1,
           enemy: prevState.score.enemy,
-        }
-      }))
+        },
+      }));
     } else if (pAttr < cardAttr) {
       this.winner('Sashay away', 'lose');
-      this.setState(prevState => ({
-        score: { 
+      this.setState((prevState) => ({
+        score: {
           player: prevState.score.player,
           enemy: prevState.score.enemy + 1,
-        }
-      }))
+        },
+      }));
     } else {
       this.winner('Shantay you both stay! <3', 'draw');
-      this.setState(prevState => ({
-        score: { 
+      this.setState((prevState) => ({
+        score: {
           player: prevState.score.player + 1,
           enemy: prevState.score.enemy + 1,
-        }
-      }))
+        },
+      }));
     }
   };
 
@@ -89,20 +88,20 @@ export default class Game extends Component {
       hideEnemyCard: false,
     });
     switch (attrType) {
-      case 'carisma':
-        this.fight(playerAttr, cardAttr1);
-        break;
-      case 'uniqueness':
-        this.fight(playerAttr, cardAttr2);
-        break;
-      case 'nerve':
-        this.fight(playerAttr, cardAttr3);
-        break;
-      case 'talent':
-        this.fight(playerAttr, cardAttr4);
-        break;
-      default:
-        return 'Error: No drag queens found!';
+    case 'carisma':
+      this.fight(playerAttr, cardAttr1);
+      break;
+    case 'uniqueness':
+      this.fight(playerAttr, cardAttr2);
+      break;
+    case 'nerve':
+      this.fight(playerAttr, cardAttr3);
+      break;
+    case 'talent':
+      this.fight(playerAttr, cardAttr4);
+      break;
+    default:
+      return 'Error: No drag queens found!';
     }
   };
 
@@ -120,7 +119,7 @@ export default class Game extends Component {
       cards: {
         playerCard,
         enemyCard,
-      }
+      },
     } = this.state;
     const {
       cardName: pCardName,
@@ -148,76 +147,114 @@ export default class Game extends Component {
     return (
       <>
         <div className="result">
-          <h2 className={cond}>{msg}</h2>
+          <h2 className={ cond }>{msg}</h2>
         </div>
         <div className="flex__container">
-          <button onClick={() => this.startGame()} className="save__button play__button">
+          <button
+            onClick={ () => this.startGame() }
+            className="save__button play__button"
+          >
             {play}
           </button>
         </div>
         <div>
           {contestantsReady ? (
-            <>
-              <div className="flex__container">
-                <div>
-                  <p>Placar:</p>
-                  <p>Jogador(a):{player}</p>
-                  <p>Computador:{enemy}</p>
-                </div>
-                <Card
-                  cardName={pCardName}
-                  cardDescription={pCardDescription}
-                  cardAttr1={pCardAttr1}
-                  cardAttr3={pCardAttr3}
-                  cardAttr2={pCardAttr2}
-                  cardAttr4={pCardAttr4}
-                  cardImage={pCardImage}
-                  cardRare={pCardRare}
-                  cardTrunfo={pCardTrunfo}
-                  compareAttr={this.compareAttr}
-                  disableButton={msg}
-                />
-                <Card
-                  cardName={cardName}
-                  cardDescription={cardDescription}
-                  cardAttr1={cardAttr1}
-                  cardAttr2={cardAttr2}
-                  cardAttr3={cardAttr3}
-                  cardAttr4={cardAttr4}
-                  cardImage={cardImage}
-                  cardRare={cardRare}
-                  cardTrunfo={cardTrunfo}
-                  hideEnemyCard={hideEnemyCard}
-                />
+            <div className="flex__container">
+              <div>
+                <p>Placar:</p>
+                <p>
+                  Jogador(a):
+                  {player}
+                </p>
+                <p>
+                  Computador:
+                  {enemy}
+                </p>
               </div>
-            </>
+              <Card
+                cardName={ pCardName }
+                cardDescription={ pCardDescription }
+                cardAttr1={ pCardAttr1 }
+                cardAttr3={ pCardAttr3 }
+                cardAttr2={ pCardAttr2 }
+                cardAttr4={ pCardAttr4 }
+                cardImage={ pCardImage }
+                cardRare={ pCardRare }
+                cardTrunfo={ pCardTrunfo }
+                compareAttr={ this.compareAttr }
+                disableButton={ msg }
+              />
+              <Card
+                cardName={ cardName }
+                cardDescription={ cardDescription }
+                cardAttr1={ cardAttr1 }
+                cardAttr2={ cardAttr2 }
+                cardAttr3={ cardAttr3 }
+                cardAttr4={ cardAttr4 }
+                cardImage={ cardImage }
+                cardRare={ cardRare }
+                cardTrunfo={ cardTrunfo }
+                hideEnemyCard={ hideEnemyCard }
+              />
+            </div>
           ) : (
             ''
           )}
         </div>
         <section className="game__rules">
-                <h3>Regras</h3>
-                <ol>
-                  O jogo já possui um baralho previamente criado, porém você pode adicionar cartas
-                  personalizadas clicando no botão "Editar Deck" acima!
-                  <li>
-                    Escolha um atributo (Charisma, Uniqueness Nerve ou Talent) de sua carta, virada para cima. O valor deste atributo será comparado ao da carta oponente, virada para baixo.
-                  </li>
-                  <li>O valor máximo que um atributo pode ter é 90.</li>
-                  <li>
-                    Vence a carta cujo atributo escolhido tiver o maior valor.
-                  </li>
-                  <li>Em caso de empate, ambos ganham pontos!</li>
-                </ol>
-                Divirta-se!
-              </section>
+          <h3>Regras</h3>
+          <ol>
+            O jogo já possui um baralho previamente criado, porém você pode adicionar
+            cartas personalizadas clicando no botão &quot;Editar Deck&quot; acima!
+            <li>
+              Escolha um atributo (Charisma, Uniqueness Nerve ou Talent) de sua carta,
+              virada para cima. O valor deste atributo será comparado ao da
+              carta oponente, virada para baixo.
+            </li>
+            <li>O valor máximo que um atributo pode ter é 90.</li>
+            <li>
+              Vence a carta cujo atributo escolhido tiver o maior valor.
+            </li>
+            <li>Em caso de empate, ambos ganham pontos!</li>
+          </ol>
+          Divirta-se!
+        </section>
       </>
     );
   }
 }
 
 Game.propTypes = {
-  cards: PropTypes.shape({
-    sort: PropTypes.func,
-  }).isRequired,
+  cards: PropTypes.arrayOf(PropTypes.shape({
+    cardName: PropTypes.string.isRequired,
+    cardDescription: PropTypes.string.isRequired,
+    cardAttr1: PropTypes.string.isRequired,
+    cardAttr2: PropTypes.string.isRequired,
+    cardAttr3: PropTypes.string.isRequired,
+    cardAttr4: PropTypes.string.isRequired,
+    cardImage: PropTypes.string.isRequired,
+    cardRare: PropTypes.string.isRequired,
+    cardTrunfo: PropTypes.bool.isRequired,
+    hasTrunfo: PropTypes.bool.isRequired,
+    isSaveButtonDisabled: PropTypes.bool.isRequired,
+    savedCardsArr: PropTypes.arrayOf(PropTypes.shape({
+      cardAttr1: PropTypes.string.isRequired,
+      cardAttr2: PropTypes.string.isRequired,
+      cardAttr3: PropTypes.string.isRequired,
+      cardAttr4: PropTypes.string.isRequired,
+      cardDescription: PropTypes.string.isRequired,
+      cardImage: PropTypes.string.isRequired,
+      cardName: PropTypes.string.isRequired,
+      cardRare: PropTypes.string.isRequired,
+      cardTrunfo: PropTypes.bool.isRequired,
+      compareAttr: PropTypes.func,
+      disableButton: PropTypes.string,
+      hideEnemyCard: PropTypes.bool,
+    })).isRequired,
+    filterQuery: PropTypes.string.isRequired,
+    filterRarity: PropTypes.arrayOf(PropTypes.string).isRequired,
+    filterTrunfo: PropTypes.bool.isRequired,
+    deckEdit: PropTypes.bool.isRequired,
+    gameState: PropTypes.string.isRequired,
+  })).isRequired,
 };
